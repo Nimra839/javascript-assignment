c3b6b33a349d27475733ca3a1880e4e659420a0d
GitWizard <nimraghouri97@gmail.com>
2024-09-20 15:17:02 -0700

    Add files via upload

---
+++ assignment.js
@@ -0,0 +1,72 @@
+
+//  QUESTION NO 1
+
+
+ALERT
+
+
+
+//  QUESTION NO 02
+
+
+
+ var firstname = "NIMRA SARFRAZ"
+ alert (firstname)
+ 
+
+
+
+ var lastname = "GHOURI"
+ alert (lastname)
+
+
+
+ var fathername ="SARFRAZ AKHTER"
+ alert (fathername)
+
+
+
+
+ alert (age)
+ alert ("my name"+" "+ age +" "+"years old" )
+
+
+ var age = 22
+ var aboutme = "I am a designer"
+ alert(`My name is nimra. ${aboutme} . I am ${age} years old. `)
+
+
+ //  QUESTION NO 03
+    
+
+
+var a = 4
+var b = 2
+ console.log (a++ + --b  - --a + b++ + a + b);
+
+ 
+            
+//  question no 05
+
+
+
+
+ var age = 25;
+
+ if (age < 13) {
+     console.log("You are a child.");
+ } else if (age >= 13 && age <= 19) {
+     console.log("You are a teenager.");
+ } else if (age >= 20 && age <= 64) {
+     console.log("You are an adult.");
+ } else {
+     console.log("You are a senior citizen.");
+ }
+
+
+
+
+ 
+
+
+ 
